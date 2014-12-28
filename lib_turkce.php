0103cfa866be6386e5fb952df43630307be234b7
mertcanekren <mertcanekren@windowslive.com>
2014-12-28 15:06:16 +0200

    büyük ve küçük harf fonksiyonları eklendi

--- lib_turkce.php
+++ lib_turkce.php
@@ -1,11 +1,13 @@
 <?php
+
 class turkce{
-    public function kucuk_harf(){
-        
-        return sonuc;
+    public function kucuk_harf($str){
+		$sonuc = mb_strtolower($str, 'UTF-8');
+        return $sonuc;
     }
-    public function buyuk_harf(){
-        
+    public function buyuk_harf($str){
+    	$sonuc = mb_strtoupper($str, 'UTF-8');
+        return $sonuc;
     }
     public function cumle_duzeni(){
         
